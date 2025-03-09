c46192ce81264d0d8c58069d365bba7714d4927b
tumminia <tumminia.p@outlook.it>
2025-03-09 22:18:01 +0100

    Update file_controller.rb
    Query by database giacenza.db and table frigorifero

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,4 +1,15 @@
+require "sqlite3"
+
 class FileController < ApplicationController
-  def index
+  def sqlite3
+    db_path = Rails.root.join("db", "giacenza.db").to_s
+    db = SQLite3::Database.new(db_path)
+    db.results_as_hash = true
+
+    @frigorifero = db.execute("SELECT * FROM frigorifero")
+
+    db.close
+
+    Rails.logger.info "📢 Dati dal database: #{@frigorifero.inspect}"
   end
 end
