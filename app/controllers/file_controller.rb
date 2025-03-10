e971f9a792aa6d88227f6a5f27306ff87bc73460
tumminia <tumminia.p@outlook.it>
2025-03-10 19:58:03 +0100

    Update file_controller.rb
    piatti function queries the database "giacenza.db" and table "piatti" and creates a URI of a json file

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,4 +1,5 @@
 require "sqlite3"
+require "json"
 
 class FileController < ApplicationController
   def sqlite3
@@ -10,6 +11,17 @@ class FileController < ApplicationController
 
     db.close
 
-    Rails.logger.info "📢 Dati dal database: #{@frigorifero.inspect}"
+    # Rails.logger.info "📢 Dati dal database: #{@frigorifero.inspect}"
+  end
+
+  def piatti
+    db_path = Rails.root.join("db", "giacenza.db").to_s
+    db = SQLite3::Database.new(db_path)
+    db.results_as_hash = true
+
+    @piatti = db.execute("SELECT * FROM piatti")
+
+    db.close
+    render json: @piatti
   end
 end
