d5c27ef27e6d83cf2b87ebac767d0d18d83b283e
tumminia <tumminia.p@outlook.it>
2025-03-16 19:48:53 +0100

    Add files via upload

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var counter = 0;
+class App {
+    carrello(a) {
+        let x = "";
+        if (10 > counter) {
+            var b = 1e18 * Math.random();
+            x = "<li id='" + b + "'>" + a.id + " <i onclick='object.clear(" + b + ");' class='bi bi-x text-danger'></i></li>",
+            $("#carrello").find("ol").append(x),
+            counter++
+        } else 
+            alert("Hai superato " + counter + " ordini")
+    }
+    clear(a) {
+        document.getElementById(a).remove(),
+        counter--;
+    }
+}
+const object = new App;
